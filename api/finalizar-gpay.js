a7e6b88ddf7207695589a5b1448529e91f184bef
victorloirogames-tech <victorloirogames@gmail.com>
2026-04-29 17:53:35 -0300

    Update finalizar-gpay.js

--- api/finalizar-gpay.js
+++ api/finalizar-gpay.js
@@ -1,6 +1,4 @@
-// /api/finalizar-gpay.js
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -31,8 +29,9 @@ export default async function handler(req, res) {
           token: token,
           description: "Pedido Google Pay Drica",
           installments: 1,
+          payment_method_id: "visa",
           payer: {
-            email: "cliente@dricaconfeitaria.com"
+            email: "cliente@email.com"
           }
         })
       }
@@ -47,4 +46,4 @@ export default async function handler(req, res) {
       error: error.message
     });
   }
-}
+};
